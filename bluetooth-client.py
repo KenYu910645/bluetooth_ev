cb717f0fae2539fae773aed30298eed7b8d6aa8f
spiderkiller13 <luckyyu910645@gmail.com>
2018-11-23 12:27:06 +0800

    test

--- bluetooth-client.py
+++ bluetooth-client.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 
 from bluetooth_template import BLUE_COM
+import signal 
 import time 
+
+is_running = True
+def sigint_handler(signum, frame):
+    global is_running
+    is_running = False
+    print('[sigint_handler] catched interrupt signal!')
+signal.signal(signal.SIGINT, sigint_handler)
+signal.signal(signal.SIGHUP, sigint_handler)
+signal.signal(signal.SIGTERM, sigint_handler)
 
 WAIT_AWK_MAX_TIME = 3 # sec 
 
 blue_com = BLUE_COM()
 
-while blue_com.connect('B8:27:EB:51:BF:F5', 3) == False :
+
+
+
+while blue_com.connect('B8:27:EB:51:BF:F5', 3) == False and is_running :
     time.sleep(1)
     print ("[Main] Reconected.")
 
@@ -15,7 +28,7 @@ while blue_com.connect('B8:27:EB:51:BF:F5', 3) == False :
 
 mid = blue_com.send("hello world")
 ts = time.time()
-while time.time() - ts < wait_awk_max_time: 
+while time.time() - ts < WAIT_AWK_MAX_TIME: 
     if blue_com.is_awk(mid): # pop out 
         print ("[main] get awk ! send completed (" + mid + ")")
         break
@@ -24,7 +37,7 @@ while time.time() - ts < wait_awk_max_time:
 
 mid = blue_com.send("hello world")
 ts = time.time()
-while time.time() - ts < wait_awk_max_time: 
+while time.time() - ts < WAIT_AWK_MAX_TIME: 
     if blue_com.is_awk(mid): # pop out 
         print ("[main] get awk ! send completed (" + mid + ")")
         break
